4d24af19551e1216eb4ec4f3aa0ad4978bf75693
LanaSteen <l4n.steen@gmail.com>
2024-09-22 22:31:59 +0400

    Add project files.

---
+++ array/Program.cs
@@ -0,0 +1,175 @@
+﻿namespace array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region array
+            //int[] num = new int[3];  // = [0,0,0]
+            //num = [10, 20, 30];
+            //num[0] = 30;
+
+
+
+
+            //int[] numbers2 = { 10, 20, 30 };
+
+            //Console.WriteLine();
+
+
+            //string name = default;
+
+
+
+            //int[] numebrs = [20, 60, 30];
+            //for (int i =0; i<numebrs.Length; i++)
+            //{
+            //    Console.WriteLine(numebrs[i]);
+            //}
+
+
+
+            //foreach (int el in numebrs)
+            //{
+            //    Console.WriteLine(el);
+            //}
+            #endregion array
+
+            #region random
+
+
+            //Random random = new Random();
+            //Console.WriteLine(random.Next(0,100));
+            //Console.WriteLine(random.NextDouble());
+            //Console.WriteLine(random.NextSingle());
+
+
+            //Random random = new Random();
+            //char c = Convert.ToChar(random.Next(33, 127));
+            //Console.WriteLine(c);
+            //int[] numArr = new int [5];
+            //for (int i =0; i<numArr.Length; i++)
+            //{
+            //    numArr[i] = random.Next(0,100);
+
+            //}
+
+            //foreach (int el in numArr)
+            //{
+            //    Console.WriteLine(el);
+            //}
+
+
+            #endregion random
+
+
+            #region classWork
+
+
+
+
+            //   •	შემთხვევითი რიცხვებით შევსებულ მასივში მოძებნეთ დადებითი რიცხვების რაოდენობა.
+            //Random random = new Random();
+            //int[] numArr = new int[10];
+            //for (int i = 0; i < numArr.Length; i++)
+            //{
+            //    numArr[i] = random.Next(-100, 100);
+
+            //}
+            //for (int i = 0; i < numArr.Length; i++)
+            //{
+            //    Console.Write($"{numArr[i]}, ");
+            //}
+            //Console.WriteLine();
+            //foreach (int el in numArr)
+            //{
+            //    if (el > 0)
+            //    {
+            //        Console.Write(el);
+            //    }
+            //}
+
+
+
+
+            //•	შეამოწმეთ შემთხვევითი რიცხვებით შევსებული მასივი შეიცავს თუ არა ისეთ 7 - ის ჯერად რიცხვს რომელიც მეტია 14 - ზე.
+
+
+            //Random random = new Random();
+            //int[] numArr = new int[10];
+            //for (int i = 0; i < numArr.Length; i++)
+            //{
+            //    numArr[i] = random.Next(49, 51);
+
+            //}
+            //for (int i = 0; i < numArr.Length; i++)
+            //{
+            //    Console.Write($"{numArr[i]}, ");
+            //}
+            //Console.WriteLine();
+            //foreach (int el in numArr)
+            //{
+            //    if (el % 7 == 0 && el > 14)
+            //    {
+            //        Console.Write($"{el}, ");
+            //    }
+            //}
+
+
+
+            //-----------------------------
+            //•	მოცემულია მასივი სადაც ჩაწერილია მომხმარებლების სახელები მაგალითად: [„ნიკა“,“გიორგი“,“ელენე“,“დავითი“] ამოიკითხეთ მასივიდან n რაოდენობის მონაცემები და დაბეჭდეთ კონსოლში შებრუნებული თანმიმდევრობით. n არის ნებისმიერი მთელი რიცხვი რომელიც არ აღემატება მასივის ზომას.
+
+            //string[] names = ["Daviti", "Nika", "Elene", "Giorgi"];
+            //Console.WriteLine($"Write number from 0 to {names.Length}");
+            //string userNum = Console.ReadLine();
+            //int.TryParse(userNum, out int userNumber);
+            //try
+            //{
+            //    for (int i = userNumber - 1; i >= 0; i--)
+            //    {
+            //        Console.WriteLine(names[i]);
+            //    }
+            //}
+            //catch (Exception e)
+            //{
+            //    Console.WriteLine(e.Message);
+            //}
+
+            //----------------------
+            #endregion classWork
+
+
+            #region  2DArray
+            int[] firstCollction = [10, 60, 30,];
+            int[][] secondCollectoin = new int[2][];
+
+
+            secondCollectoin[0] = [10, 60,  30,];
+            secondCollectoin[1] = [3,  20,  9];
+
+
+            //int sum = secondCollectoin[0][0] + secondCollectoin[1][secondCollectoin[1].Length - 1];
+            //Console.WriteLine(sum);
+
+
+
+            for (int i =0; i<secondCollectoin.Length; i++)
+            {
+                int maxInRow = 0;
+                for (int j =0; j < secondCollectoin[i].Length; j++)
+                {
+                    if (secondCollectoin[i][j] > maxInRow)
+                    {
+                        maxInRow = secondCollectoin[i][j];
+                    }
+                   
+                }
+                Console.WriteLine(maxInRow);
+            }
+
+            #endregion  2DArray
+        }
+    }
+}
---
+++ homeWork/Program.cs
@@ -0,0 +1,137 @@
+﻿namespace array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //დავალება 1
+            //•	დაწერეთ პროგრამა რომელიც კონსოლში დაბეჭდავს მასივში შემავალი ყველა ლუწი ელემენტის ჯამს.
+
+            //Console.WriteLine("Enter number...");
+            //string userNum = Console.ReadLine();
+            //int.TryParse(userNum, out int userNumber);
+
+
+            //Random random = new Random();
+            //int[] numArr = new int[userNumber];
+            //for (int i = 0; i < numArr.Length; i++)
+            //{
+            //    numArr[i] = random.Next(0, 100);
+
+            //}
+            //for (int i = 0; i < numArr.Length; i++)
+            //{
+            //    Console.Write($"{numArr[i]}, ");
+            //}
+            //Console.WriteLine();
+            //int sumOfEven = 0;
+            //foreach (int el in numArr)
+            //{
+            //    if (el %2==0)
+            //    {
+            //        sumOfEven += el;
+            //    }
+            //}
+            //Console.WriteLine($"Sum of even numbers is {sumOfEven}");
+
+            //დავალება 2
+            //•	დაწერეთ პროგრამა რომელიც ერთ მასივიდან მეორეში გადააკოპირებს ყველა ელემენტს (ჩათვალეთ რომ ორივე მასივის ზომა ერთი და იგივეა).
+            //int[] firstArr = [20, 60, 30, 15];
+            //int[] secondArr = new int[firstArr.Length];
+
+            //for (int i =0; i<firstArr.Length; i++)
+            //{
+            //    secondArr[i] = firstArr[i];
+            //}
+            //foreach (int el in secondArr)
+            //{
+            //    Console.Write($"{el} ");
+            //}
+
+
+            //დავალება 3
+            //•	დაწერეთ პროგრამა რომელიც კონსოლში დაბეჭდავს მხოლოდ მასივის უნიკალურ ელემენტებს ანუ ისეთ ელემენტებს რომლებიც არსებულ მასივში მხოლოდ ერთხელ გვხვდებიან.
+            //int[] arr = [20, 60, 30, 15, 20, 60, 30, 40];
+
+            //for (int i = 0; i < arr.Length; i++) {
+            //    int count = 0;
+            //    for (int j = 0; j<arr.Length; j++) 
+            //    {
+            //        if (arr[i] == arr[j])
+            //        {
+            //            count++;
+
+            //        }
+            //    }
+            //    if (count >=2)
+            //    {
+            //        continue;
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine(arr[i]);
+            //    }
+            //}
+
+            //დავალება 4
+            //•	დაწერეთ პროგრამა რომელიც მასივში მოძებნის მაქსიმალურ ელემენტს.
+
+            //int[] arr = [20, 200, 30, 15, 20, -10, 30, 100];
+            //int max = arr[0];
+            //for (int i = 1; i < arr.Length; i++)
+            //{
+            //    if (arr[i] > max)
+            //    {
+            //        max = arr[i];
+            //    }
+            //}
+            //Console.WriteLine(max);
+
+
+            //დავალება 5
+            //•	დაწერეთ პროგრამა რომელიც მასივში მოძებნის მაქსიმალური ელემენტის ინდექსს.
+            //int[] arr = [20, 200, 30, 1500, 20, -10, 30, 100];
+            //int maxElIndex = 0;
+            //int max = arr[0];
+            //for (int i = 1; i < arr.Length; i++)
+            //{
+            //    if (arr[i] > max)
+            //    {
+            //        max = arr[i];
+            //        maxElIndex = i;
+            //    }
+            //}
+            //Console.WriteLine(maxElIndex);
+
+
+
+            //დავალება 6
+            //•	დაალაგეთ მოცემული მასივი ზრდადობის მიხედვით. (არ გამოიყენოთ C#-ში არსებული მზა ფუნქციები მაგალითად OrderBy ან რაიმე მსგავსი სრული ფუნქციონალი დაწერეთ ხელით)
+
+            int[] arr = [20, 200, 30, 1500, 20, -10, 30, 100];
+
+            foreach (int element in arr)
+            {
+                for (int i = 0; i < arr.Length - 1; i++)
+                {
+                    if (arr[i] > arr[i + 1])
+                    {
+                        int temp = arr[i];
+                        arr[i] = arr[i + 1];
+                        arr[i + 1] = temp;
+                    }
+                }
+
+               
+            }
+            foreach (int el in arr)
+            {
+                Console.Write($"{el} ");
+            }
+
+        }
+    }
+}
+
+
